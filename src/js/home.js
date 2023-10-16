5821875f9820cc3c89bfdd7d6215c9905b811055
vlduggen <vitor.duggen@amaiseducacao.com.br>
2023-10-16 16:29:57 -0300

    Refatorado home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,9 @@
+function getInputUpload() {
+  return document.getElementById("rf-upload");
+}
+
 function openUpload() {
-  document.getElementById("rf-upload").click();
+  getInputUpload().click();
 }
 
 function changeTab(contentFile) {
@@ -8,23 +12,36 @@ function changeTab(contentFile) {
   createStructure(document.getElementById("json-tree"));
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("rf-upload").addEventListener("change", async (e) => {
-    const fileReader = new FileReader();
-    fileReader.onload = function () {
-      try {
-        JSON.parse(fileReader.result);
-      } catch (e) {
-        console.log(e);
-        document.getElementById('rf-error').classList.remove('rf-error--hidden');
-        return false;
-      }
+function setNameFile(nameFile) {
+  document.querySelector(".rf-main #json-tree .rf-title").innerText = nameFile;
+}
 
-      changeTab(fileReader.result);
-      renderTreeView(JSON.parse(fileReader.result));
-    };
-    document.querySelector(".rf-main #json-tree .rf-title").innerText =
-      e.target.files[0].name;
-    fileReader.readAsText(e.target.files[0]);
-  });
+function loadFileReader(fileReader) {
+  try {
+    JSON.parse(fileReader.result);
+  } catch (e) {
+    console.log(e);
+    document.getElementById("rf-error").classList.remove("rf-error--hidden");
+    return false;
+  }
+
+  changeTab(fileReader.result);
+  renderTreeView(JSON.parse(fileReader.result));
+}
+
+function changeInputUpload(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function () {
+    loadFileReader(fileReader);
+  };
+  const arrFiles = event.target.files;
+  if (arrFiles && arrFiles.length > 0) {
+    const objFirstFile = arrFiles[0];
+    setNameFile(objFirstFile.name);
+    fileReader.readAsText(objFirstFile);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  getInputUpload().addEventListener("change", (event) => changeInputUpload(event));
 });
