cf5d6edff7f865ce766b4c015d2eaa0e7e3301c8
vlduggen <vitor.duggen@amaiseducacao.com.br>
2023-10-16 16:31:47 -0300

    Refatorado home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+  getInputUpload().addEventListener("change", (event) => changeInputUpload(event));
+});
+
 function getInputUpload() {
   return document.getElementById("rf-upload");
 }
@@ -6,14 +10,10 @@ function openUpload() {
   getInputUpload().click();
 }
 
-function changeTab(contentFile) {
+function changeTab() {
   document.getElementById("home").style.display = "none";
   document.getElementById("json-tree").style.display = "flex";
   createStructure(document.getElementById("json-tree"));
-}
-
-function setNameFile(nameFile) {
-  document.querySelector(".rf-main #json-tree .rf-title").innerText = nameFile;
 }
 
 function loadFileReader(fileReader) {
@@ -24,9 +24,12 @@ function loadFileReader(fileReader) {
     document.getElementById("rf-error").classList.remove("rf-error--hidden");
     return false;
   }
-
   changeTab(fileReader.result);
   renderTreeView(JSON.parse(fileReader.result));
+}
+
+function setTitle(nameFile) {
+  document.querySelector(".rf-main #json-tree .rf-title").innerText = nameFile;
 }
 
 function changeInputUpload(event) {
@@ -37,11 +40,7 @@ function changeInputUpload(event) {
   const arrFiles = event.target.files;
   if (arrFiles && arrFiles.length > 0) {
     const objFirstFile = arrFiles[0];
-    setNameFile(objFirstFile.name);
+    setTitle(objFirstFile.name);
     fileReader.readAsText(objFirstFile);
   }
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  getInputUpload().addEventListener("change", (event) => changeInputUpload(event));
-});
