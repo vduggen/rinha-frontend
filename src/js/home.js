a1bb5e99ec6ee086d8efc1dc3aa952a8370871d0
vlduggen <vitor.duggen@amaiseducacao.com.br>
2023-10-16 16:21:34 -0300

    Feito tratamento de arquivo com JSON inválido

--- src/js/home.js
+++ src/js/home.js
@@ -8,15 +8,23 @@ function changeTab(contentFile) {
   createStructure(document.getElementById("json-tree"));
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("rf-upload").addEventListener("change", async (e) => {
-        const fileReader = new FileReader();
-        fileReader.onload = function () {
-          changeTab(fileReader.result);
-          renderTreeView(JSON.parse(fileReader.result));
-        };
-        document.querySelector(".rf-main #json-tree .rf-title").innerText =
-          e.target.files[0].name;
-        fileReader.readAsText(e.target.files[0]);
-      });
-})
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("rf-upload").addEventListener("change", async (e) => {
+    const fileReader = new FileReader();
+    fileReader.onload = function () {
+      try {
+        JSON.parse(fileReader.result);
+      } catch (e) {
+        console.log(e);
+        document.getElementById('rf-error').classList.remove('rf-error--hidden');
+        return false;
+      }
+
+      changeTab(fileReader.result);
+      renderTreeView(JSON.parse(fileReader.result));
+    };
+    document.querySelector(".rf-main #json-tree .rf-title").innerText =
+      e.target.files[0].name;
+    fileReader.readAsText(e.target.files[0]);
+  });
+});
